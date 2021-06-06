8df712d60689857145f1cba6d5cea423f0b86d67
Stasiak.Jacek <222579@edu.p.lodz.pl>
2021-06-06 13:45:27 +0200

    Create user test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -16,13 +16,43 @@ class CreateUserTest extends FunctionalTests {
     void createUserWithProperDataReturnsCreatedStatus() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
         given().accept(ContentType.JSON)
-               .header("Content-Type", "application/json;charset=UTF-8")
-               .body(jsonObj.toString())
-               .expect()
-               .log()
-               .all()
-               .statusCode(HttpStatus.SC_CREATED)
-               .when()
-               .post(USER_API);
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(USER_API);
+    }
+
+    @Test
+    void createDuplicateUser_shouldResultIn409ResponseStatus() {
+        JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CONFLICT)
+                .when()
+                .post(USER_API);
+    }
+
+    @Test
+    void getDataOfNonexistentUser_shouldResultIn404Status(){
+//        JSONObject jsonObj = new JSONObject().put("id", "19");
+        int id = 19;
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+//                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+//                .statusCode(HttpStatus.SC_NOT_FOUND)
+                .when()
+                .get("/blog/user/" + id);
     }
 }
